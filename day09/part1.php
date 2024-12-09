c7fd07711bf6f4ed1e9d87cd33a9d7cf0e8d6bfd
Huntly Cameron <huntly.cameron@gmail.com>
2024-12-09 09:05:29 +0000

    day 9, p1 and start of p2

---
+++ day09/part1.php
@@ -0,0 +1,209 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AOC\D09\P1;
+use SplDoublyLinkedList;
+
+
+/**
+ * Class Block
+ */
+class Block{
+    /**
+     * @var int
+     **/
+    public $label;
+
+    function __construct(int $label){
+        $this->label = $label;
+    }
+}
+
+
+class FileSystem{
+    /**
+     * @var SplDoublyLinkedList
+     **/
+    private $blocks;
+
+    public function __construct(){
+        $this->blocks = new SplDoublyLinkedList();
+    }
+
+    public function defragCompact(){
+        $this->blocks->setIteratorMode(SplDoublyLinkedList::IT_MODE_FIFO);
+
+
+        $tail = 0;
+        $head = $this->blocks->count() - 1;
+        for ($this->blocks->rewind(); $this->blocks->valid(); $this->blocks->next()) {
+            if($tail >= $head) break;
+
+            echo "Head: $head, Tail: $tail" . PHP_EOL;
+            $block = $this->blocks->current();
+
+            echo "checking block: {$block->label} against " . $this->blocks->offsetGet($head)->label . PHP_EOL;
+
+
+            if($block->label == -1){
+                do{
+                    $endBlock = $this->blocks->offsetGet($head);
+                    if($endBlock->label != -1) break;
+                    $head--;
+                }while($endBlock->label == -1 && $head > $tail);
+
+                // move end block here
+                $this->blocks->offsetSet($tail, $endBlock);
+
+                // set end block to free space
+                $this->blocks->offsetSet($head, new Block(-1));
+
+                $head--;
+            }
+            $tail++;
+        }
+    }
+
+    public function defragRetainingWholeFiles(){
+        $this->blocks->setIteratorMode(SplDoublyLinkedList::IT_MODE_FIFO);
+
+        $tail = 0;
+        $head = $this->blocks->count() - 1;
+
+        // get first block of free space
+        $freeSpaceStart = -1;
+        $freeSpaceEnd = -1;
+        for ($this->blocks->rewind(); $this->blocks->valid(); $this->blocks->next()) {
+            if($this->blocks->current()->label == -1){
+                $freeSpaceStart = $this->blocks->key();
+            }else{
+                $freeSpaceEnd = $this->blocks->key();
+                break;
+            }
+        }
+
+        // get last file start and end
+        $fileStart = -1;
+        $fileEnd = -1;
+        $fileLabel = -1;
+
+        $this->blocks->setIteratorMode(SplDoublyLinkedList::IT_MODE_LIFO);
+        for ($this->blocks->rewind(); $this->blocks->valid(); $this->blocks->next()) {
+            if($this->blocks->current()->label != -1){
+                $fileLabel = $this->blocks->current()->label;
+                $fileStart = $this->blocks->key();
+                break;
+            }
+        }
+
+
+    }
+
+
+    public function checksum(){
+        $checksum = 0;
+        $this->blocks->setIteratorMode(SplDoublyLinkedList::IT_MODE_FIFO);
+
+        $sumMultiplier = 0;
+        for ($this->blocks->rewind(); $this->blocks->valid(); $this->blocks->next()) {
+            if($this->blocks->current()->label == -1) continue;
+            $checksum += $sumMultiplier * $this->blocks->current()->label;
+            $sumMultiplier++;
+        }
+        return $checksum;
+    }
+
+    static function fromMap(string $map){
+        $fs = new FileSystem();
+
+        if($map === '') return $fs;
+
+        $map = str_split(trim($map));
+
+        $isFreeSpace = false;
+        $fileLabel = 0;
+
+        foreach($map as $m){
+            $label = $isFreeSpace ? -1 : $fileLabel;
+            $size = intval($m);
+
+            for($i = 0; $i < $size; $i++){
+                $fs->blocks->push(new Block((int)$label));
+            }
+
+            if($label >= 0) $fileLabel++;
+            $isFreeSpace = !$isFreeSpace;
+        }
+
+        return $fs;
+    }
+
+    static function fromString(string $str){
+        $fs = new FileSystem();
+
+        if($str === '') return $fs;
+
+        $str = str_split(trim($str));
+
+        foreach($str as $s){
+            $label = $s == '.' ? -1 : (int)$s;
+            $fs->blocks->push(new Block($label));
+        }
+
+        return $fs;
+    }
+
+    public function __toString(){
+        $str = '';
+        foreach($this->blocks as $block){
+            $label = $block->label == -1 ? '.' : $block->label;
+            $str .= $label;
+        }
+        return $str;
+    }
+}
+
+
+class Part1{
+    static function run(){
+        $input = file_get_contents(__DIR__ . '/test-input.txt');
+        if($input === false) exit("Input file not found" . PHP_EOL);
+
+        $fs = FileSystem::fromMap($input);
+
+        echo "{$fs}" . PHP_EOL;
+        echo "Defragging..." . PHP_EOL;
+        $fs->defragCompact();
+
+        echo "{$fs}" . PHP_EOL;
+
+        $checksum = $fs->checksum();
+
+
+        echo "Part 1: $checksum" . PHP_EOL;
+    }
+}
+
+class Part2{
+    static function run(){
+        $input = file_get_contents(__DIR__ . '/test-input.txt');
+        if($input === false) exit("Input file not found" . PHP_EOL);
+
+        $fs = FileSystem::fromMap($input);
+
+        echo "{$fs}" . PHP_EOL;
+        echo "Defragging..." . PHP_EOL;
+        $fs->defragRetainingWholeFiles();
+
+        echo "{$fs}" . PHP_EOL;
+
+        $checksum = $fs->checksum();
+
+
+        echo "Part 2: $checksum" . PHP_EOL;
+    }
+}
+
+//Part1::run();
+Part2::run();
